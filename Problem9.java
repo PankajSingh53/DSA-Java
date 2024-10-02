578713cb18c651af26f2bc848b1dd37f1cc02cf7
panku991 <130482879+panku991@users.noreply.github.com>
2024-10-02 23:44:46 +0530

    commit problem1.java

---
+++ Problem9.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Problem9 {
+
+    public static void main(String[] args) {
+
+
+        System.out.println(isPalindrome(12321));
+        System.out.println(isPalindrome(1221));
+        System.out.println(isPalindrome(-121));
+        System.out.println(isPalindrome(100));
+
+    }
+
+    static boolean isPalindrome(int x) {
+
+        if(x<0 || ( x%10==0 && x!=0)){
+            return false;
+        }
+
+        int reversedHalf = 0;
+
+        while(x>reversedHalf){
+            reversedHalf = reversedHalf * 10 + x%10;
+            x /=10;
+        }
+
+        return (x==reversedHalf || x==reversedHalf/10);
+
+    }
+}
